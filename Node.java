c4c6f2fec8a511b8d593ce6f81fa0bdcc734a7b1
AshCash223 <ashtess0@gmail.com>
2024-04-08 14:11:40 +1200

    first commit

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+	String  value = "";
+	Node head;
+	Node next;
+
+	public Node(String s){ //constructor
+		value = s;
+		next = null;
+	}
+}
---
+++ StrBST.java
@@ -0,0 +1,22 @@
+public class StrBST {
+
+	Node root;
+
+	public static void strBst(String[] args){
+		first.insert("A");
+	}
+
+	public void insert(String s){
+		Node newNode = new Node(s);
+		Node curr = root;
+
+		if (root == null) {
+			newNode = root;
+		}
+
+		System.out.println(root);
+
+
+	}
+
+}
